184f377644cedb20f800b2ea9a424aff982b8840
golemiam <52841560+golemiam@users.noreply.github.com>
2023-07-22 09:11:02 -0600

    Update index.py
    Added some question 3 efforts.

--- index.py
+++ index.py
@@ -1,5 +1,11 @@
 import csv
 import pandas as pd
+import requests
+import selenium
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait as wait
+
 #from pandas_datareader import data
 #from pandas.tseries import offsets
 
@@ -18,6 +24,9 @@ def run_project():
     :return:
     Returns the function chosen.
     """
+    #url_read_question_1()
+    #open_browser()
+    #opener()
     pd.set_option('display.max_columns', 22)
     question_choice = input("Which question would you like to run? ")
     if question_choice == "1":
@@ -30,23 +39,56 @@ def run_project():
         pass
 
 
-def save_to_file(data):
-    out_file_choice = input("What would you like to name the file? ")
-    try:
-        with open(out_file_choice, "x") as out_file:
-            print(data, file= out_file)
-    except:
-        out_file_placer = "placer.txt"
-        with open(out_file_placer, "w") as out_file:
-            print(data, file= out_file)
-            print("""
-                You chose a file name that has already been taken, 
-            your data has been placed in placer.txt and will be 
-            overwritten when this program runs again. Either choose 
-            a new file name where you would like this saved, or view 
-            the data now before it gets removed. 
-            """)
+def opener():
+    # the target we want to open
+    url = "https://data.cms.gov"
 
+    # open with GET method
+    resp = requests.get(url)
+
+    # http_respone 200 means OK status
+    if resp.status_code == 200:
+        print("Successfully opened the web page")
+        print("The news are as follow :-\n")
+
+        # we need a parser,Python built-in HTML parser is enough .
+        soup = BeautifulSoup(resp.text, 'html.parser')
+
+        # l is the list which contains all the text i.e news
+        l = soup.find("a", {"class": "HomepageMostViewedDatasets__button btn btn-primary"})
+        print(l)
+        # now we want to print only the text part of the anchor.
+        # find all the elements of a, i.e anchor
+        for i in l.findAll("a"):
+            print(i.text)
+    else:
+        print("Error")
+def open_browser():
+    driver = webdriver.Chrome(r"C:/Users/Golem/Downloads/chromedriver_win32/chromedriver")
+    url = "https://data.cms.gov/covid-19/covid-19-nursing-home-data/data?query=%7B%22filters%22%3A%7B%22rootConjunction%22%3A%7B%22label%22%3A%22And%22%2C%22value%22%3A%22AND%22%7D%2C%22list%22%3A%5B%5D%7D%2C%22keywords%22%3A%22%22%2C%22offset%22%3A0%2C%22limit%22%3A10%2C%22sort%22%3A%7B%22sortBy%22%3Anull%2C%22sortOrder%22%3Anull%7D%2C%22columns%22%3A%5B%22week_ending%22%2C%22federal_provider_number%22%2C%22provider_name%22%2C%22provider_city%22%2C%22provider_state%22%2C%22provider_zip_code%22%2C%22provider_phone_number%22%2C%22residents_weekly_confirmed_covid_19%22%2C%22number_of_all_healthcare_personnel_eligible_to_work_in_this_facility_for_at_least_1_day_this_week_who_received_a_completed_covid_19_vaccination_at_any_time%22%2C%22recent_percentage_of_current_healthcare_personnel_who_received_a_completed_covid_19_vaccination_at_any_time%22%2C%22percentage_of_current_healthcare_personnel_who_received_a_completed_covid_19_vaccination_at_any_time%22%2C%22percentage_of_current_healthcare_personnel_with_a_completed_vaccination_who_received_a_covid_19_vaccine_booster_at_any_time%22%2C%22Recent_Percentage_of_Current_Healthcare_Personnel_Up_to_Date_with_COVID_19_Vaccines%22%2C%22Percentage_of_Current_Healthcare_Personnel_Up_to_Date_with_COVID_19_Vaccines%22%5D%7D"
+
+    driver.get(url)
+    driver.find_element_by_link_text('export_btn_text').click()
+    text_field = driver.find_element_by_link_text('export_btn_text')
+    text = wait(driver, 10).until(lambda driver: not text_field.text == 'Export' and text_field.text)
+    return text
+
+    print(open_browser())
+
+def url_read_question_1():
+    url = "https://data.cms.gov/covid-19/covid-19-nursing-home-data/data?query=%7B%22filters%22%3A%7B%22rootConjunction%22%3A%7B%22label%22%3A%22And%22%2C%22value%22%3A%22AND%22%7D%2C%22list%22%3A%5B%5D%7D%2C%22keywords%22%3A%22%22%2C%22offset%22%3A0%2C%22limit%22%3A10%2C%22sort%22%3A%7B%22sortBy%22%3Anull%2C%22sortOrder%22%3Anull%7D%2C%22columns%22%3A%5B%22week_ending%22%2C%22federal_provider_number%22%2C%22provider_name%22%2C%22provider_city%22%2C%22provider_state%22%2C%22provider_zip_code%22%2C%22provider_phone_number%22%2C%22residents_weekly_confirmed_covid_19%22%2C%22number_of_all_healthcare_personnel_eligible_to_work_in_this_facility_for_at_least_1_day_this_week_who_received_a_completed_covid_19_vaccination_at_any_time%22%2C%22recent_percentage_of_current_healthcare_personnel_who_received_a_completed_covid_19_vaccination_at_any_time%22%2C%22percentage_of_current_healthcare_personnel_who_received_a_completed_covid_19_vaccination_at_any_time%22%2C%22percentage_of_current_healthcare_personnel_with_a_completed_vaccination_who_received_a_covid_19_vaccine_booster_at_any_time%22%2C%22Recent_Percentage_of_Current_Healthcare_Personnel_Up_to_Date_with_COVID_19_Vaccines%22%2C%22Percentage_of_Current_Healthcare_Personnel_Up_to_Date_with_COVID_19_Vaccines%22%5D%7D"
+    site = requests.get(url)
+    response = requests.get(f"{url}/Export")
+    print(site.json)
+    print(site.elapsed)
+    print(site.iter_content)
+    print(site.headers)
+    print(site.content)
+    for i in range(len(site.__dir__())):
+        print(site.__dir__()[i])
+    print(site.url)
+
+    #print(response.text)
 
 
 def read_panda_file_question_1():
@@ -251,11 +293,59 @@ def read_panda_file_question_2():
     save_to_file(data)
 
 def read_panda_file_question_3():
-    bonus_df = pd.read_csv('bonus_challenge.csv')
+    bonus_df = pd.read_csv('bonus_challenge.csv', parse_dates= ["date_last_administered"])
     #print(bonus_df)
     no_nan = bonus_df.dropna
-    nans = bonus_df-[bonus_df.dropna]
+
+    filled_nan = bonus_df.fillna(-1)
+    #print(filled_nan.index)
+    for i in range(len(filled_nan.iloc[0])):
+        #print(i)
+        pass
+    sorted_employees = filled_nan.sort_values(["employee_id"])
+    #print(sorted_employees)
+    no_dates = filled_nan["date_last_administered"] == -1
+    medically_exempt = filled_nan["medically_exempt"] == 1
+    refused = filled_nan["refused"] == 1
+    xor_group_1 = filled_nan[no_dates^medically_exempt^refused]
+    pfizer_one = filled_nan["got_pfizer1"] == 1
+    pfizer_two = filled_nan["got_pfizer2"] == 1
+    pfizer_three = filled_nan["got_pfizer3"] == 1
+    pfizer_or_group = filled_nan[pfizer_one|pfizer_two|pfizer_three]
+    moderna_one = filled_nan["got_moderna1"] == 1
+    moderna_two = filled_nan["got_moderna2"] == 1
+    moderna_three = filled_nan["got_moderna3"] == 1
+    moderna_or_group = filled_nan[moderna_one | moderna_two | moderna_three]
+    janssen_one = filled_nan["got_janssen1"] == 1
+    janssen_two = filled_nan["got_janssen2"] == 1
+    janssen_or_group = filled_nan[janssen_one | janssen_two]
+    unknown_one = filled_nan["got_unknown1"] == 1
+    unknown_two = filled_nan["got_unknown2"] == 1
+    unknown_three = filled_nan["got_unknown3"] == 1
+    unknown_or_group = filled_nan[unknown_one | unknown_two | unknown_three]
+
+    print(moderna_or_group)
+    #xor_group_2 = filled_nan[moderna_or_group ^ pfizer_or_group ^ janssen_or_group ^ unknown_or_group]
+    #This breaks it 
+    #print(xor_group_2)
+
+    #print(filled_nan[no_dates])
+    print(xor_group_1)
+
+    filled_nan["has_nans"] = "No"
+    #print(filled_nan.head(5))
+    def rows_with_nans(nan_spots):
+        if nan_spots in ["Filled"]:
+            return "yes"
+    nan_series = filled_nan["has_nans"].apply(rows_with_nans)
+    print(nan_series)
+
+
+    #print(no_nan)
+    #nans = [bonus_df-no_nan]
+    #print(nans)
     nan_date_list = []
+    """
     for line, date in zip(bonus_df, bonus_df['date_last_administered']):
         try:
             if date == '1':
@@ -266,7 +356,26 @@ def read_panda_file_question_3():
                 print(f"{line}This row lacks a date")
                 nan_date_list.append(line)
     print(nan_date_list)
-    print(nans)
+    #print(nans)
+    """
+
+def save_to_file(data):
+    out_file_choice = input("What would you like to name the file? ")
+    try:
+        with open(out_file_choice, "x") as out_file:
+            print(data, file= out_file)
+    except:
+        out_file_placer = "placer.txt"
+        with open(out_file_placer, "w") as out_file:
+            print(data, file= out_file)
+            print("""
+                You chose a file name that has already been taken, 
+            your data has been placed in placer.txt and will be 
+            overwritten when this program runs again. Either choose 
+            a new file name where you would like this saved, or view 
+            the data now before it gets removed. 
+            """)
+
 
 if __name__ == "__main__":
     main()
